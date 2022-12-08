249951f718675fe1ead9fed39786009311511fa1
Jason Mateosky <jasonmateosky@gmail.com>
2022-12-08 11:32:32 -0500

    flex stuff

--- capstone/vue/src/components/ExerciseList.vue
+++ capstone/vue/src/components/ExerciseList.vue
@@ -10,12 +10,29 @@
 
 <script>
 import ExerciseCard from './ExerciseCard.vue'
+import exercise from "../services/Exercise.js";
+
 
 export default {
     name: 'exercise-list',
     components: {
       ExerciseCard
+    },
+    created(){
+this.getExercises();
+
+}, 
+methods:
+{
+    getExercises()
+    {
+        exercise.listExercise().then(response => {
+            this.$store.commit("SET_EXERCISE", response.data)
+        
+        });
+
     }
+}
 }
 
 </script>
@@ -24,7 +41,8 @@ export default {
 .exercise-container {
     display:flex;
     justify-content: space-evenly;
-    flex-wrap: fixed;
-    
+    flex-wrap: nowrap;
+    overflow-x: auto;
+   -webkit-overflow-scrolling: touch;
 }
 </style>
